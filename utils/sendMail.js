368b536dc61c2dc6047bd338710a4f4247f99b42
Vicktor007 <vicktord007@gmail.com>
2023-11-14 19:13:43 +0100

    initial commit

---
+++ utils/sendMail.js
@@ -0,0 +1,77 @@
+// const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
+const nodemailer = require("nodemailer");
+
+
+// create email transporter
+const sendEmail = async (subject, message, send_to, sent_from, reply_to) =>{
+    const transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: 587,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+
+        },
+
+        // incase of error
+        tls: {
+            rejectUnauthorized: false
+        }
+    })
+    //  email sending options
+    const options = {
+        from: sent_from,
+        to: send_to,
+        replyTo: reply_to,
+        subject: subject,
+        html: message
+    };
+    // send email
+    transporter.sendMail(options, function(err, info) {
+        if (err) {
+            console.log(err);
+        } else {
+
+            console.log(info);
+        }
+    })
+}
+
+module.exports = sendEmail;
+
+
+// const sendEmail = async (subject, message, send_to, sent_from, reply_to) => {
+//   // Create Email Transporter
+//   const transporter = nodemailer.createTransport({
+//     host: process.env.EMAIL_HOST,
+//     port: 587,
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS,
+//     },
+//     tls: {
+//       rejectUnauthorized: false,
+//     },
+//   });
+
+//   // Option for sending email
+//   const options = {
+//     from: sent_from,
+//     to: send_to,
+//     replyTo: reply_to,
+//     subject: subject,
+//     html: message,
+//   };
+
+//   // send email
+//   transporter.sendMail(options, function (err, info) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log(info);
+//     }
+//   });
+// };
+
+// module.exports = sendEmail;
