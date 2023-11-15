4012869f7d7a39de867cf4ad94ba78b3bddfe381
Vicktor007 <vicktord007@gmail.com>
2023-11-15 21:02:57 +0100

    code clean up

--- middleWare/deletedUserMiddleware.js
+++ middleWare/deletedUserMiddleware.js
@@ -1,36 +1,3 @@
-// const asyncHandler = require("express-async-handler");
-// const User = require("../models/userModels");
-// const jwt = require("jsonwebtoken");
-
-// const protectDeletedUser = asyncHandler (async(req, res, next) => {
-//     try {
-//         const token = req.cookies.token
-//         if(!token) {
-//             res.status(401) 
-//             throw new Error("Not authorized, please login")      
-//            }
-
-//         //    verify token
-//         const verifiedToken = jwt.verify(token, process.env.JWT_SECRET)
-//         // get user id from token
-//         const user = await User.findById(verifiedToken.id).select("-password")
-
-//         if(!user) {
-//             res.status(401)
-//         throw new Error("User not found");
-    
-//         }
-//         req.user = user
-//         next()
-//     } catch (error) {
-//         res.status(401) 
-//         // throw new Error("Not authorized, please login")  
-//     }
-// })
-
-// module.exports = protectDeletedUser; 
-
-
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModels");
 const jwt = require("jsonwebtoken");
